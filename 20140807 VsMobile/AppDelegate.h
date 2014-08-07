5a02d36876d791569b18f2603a86b26ec0a15400
admin <admin@admins-Mac-mini.local>
2014-08-07 07:50:00 +0200

    Initial Commit

---
+++ 20140807 VsMobile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  20140807 VsMobile
+//
+//  Created by admin on 8/7/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 20140807 VsMobileTests/_0140807_VsMobileTests.m
@@ -0,0 +1,34 @@
+//
+//  _0140807_VsMobileTests.m
+//  20140807 VsMobileTests
+//
+//  Created by admin on 8/7/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0140807_VsMobileTests : XCTestCase
+
+@end
+
+@implementation _0140807_VsMobileTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
